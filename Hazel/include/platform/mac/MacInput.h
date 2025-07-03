80ef49c054bec07c87a5656aaa37a351c37f8b5e
dingrui <Bannirui@outlook.com>
2025-07-03 17:44:05 +0800

    input事件

---
+++ Hazel/include/platform/mac/MacInput.h
@@ -0,0 +1,22 @@
+//
+// Created by rui ding on 2025/7/3.
+//
+
+#pragma once
+
+#include "Hazel/Input.h"
+
+namespace Hazel
+{
+
+class MacInput : public Input
+{
+protected:
+    virtual bool IsKeyPressedImpl(int keycode) override;
+    virtual bool IsMouseButtonPressedImpl(int button) override;
+    virtual std::pair<float, float> GetMousePositionImpl() override;
+    virtual float GetMouseXImpl() override;
+    virtual float GetMouseYImpl() override;
+};
+
+}
---
+++ Hazel/src/platform/mac/MacInput.cpp
@@ -0,0 +1,45 @@
+//
+// Created by rui ding on 2025/7/3.
+//
+
+#include "platform/mac/MacInput.h"
+
+#include <GLFW/glfw3.h>
+#include <Hazel/Core/KeyCodes.h>
+
+#include "Hazel/Application.h"
+
+namespace Hazel
+{
+    Input* Input::s_Instance = nullptr;
+
+    bool MacInput::IsKeyPressedImpl(int keycode) {
+        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+        auto state = glfwGetKey(window, keycode);
+        return state == GLFW_PRESS || state == GLFW_REPEAT;
+    }
+
+    bool MacInput::IsMouseButtonPressedImpl(int button) {
+        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+        auto state = glfwGetMouseButton(window, button);
+        return state == GLFW_PRESS;
+    }
+
+    std::pair<float, float> MacInput::GetMousePositionImpl() {
+        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        return {(float)xpos, (float)ypos};
+    }
+
+    float MacInput::GetMouseXImpl() {
+        auto [x, y] = GetMousePositionImpl();
+        return x;
+    }
+
+    float MacInput::GetMouseYImpl() {
+        auto [x, y] = GetMousePositionImpl();
+        return y;
+    }
+
+}
